b81539d4bd21e78f474be6c9df27b5f3692ac8fa
Clau Ziemba <53158588+cziemba1@users.noreply.github.com>
2020-08-15 22:44:18 -0300

    extension methods practice

---
+++ ExtensionMethodsMiniApp/ExtensionMethods/ConsoleHelper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    public static class ConsoleHelper
+    {
+        public static string RequestString(this string message)
+        {
+            string output = "";
+
+            while (string.IsNullOrEmpty(output))
+            {
+                Console.WriteLine(message);
+                output = Console.ReadLine();
+            }
+
+            return output;
+        }
+
+        public static int RequestInt(this string messageInt)
+        {
+            return messageInt.RequestInt(false);
+        }
+
+        public static int RequestInt(this string messageInt, int minValue, int maxValue)
+        {
+            return messageInt.RequestInt(true, minValue, maxValue);
+        }
+
+        private static int RequestInt(this string messageInt, bool useMinMax, int minValue = 0, int maxValue = 0)
+        {
+            int output = 0;
+            bool isValidInt = false;
+            bool isValidRange = true;
+
+            while (isValidInt == false || isValidRange == false)
+            {
+                Console.WriteLine(messageInt);
+                isValidInt = int.TryParse(Console.ReadLine(), out output);
+
+                if (useMinMax == true)
+                {
+                    isValidRange = (output >= minValue && output <= maxValue);
+                }
+            }
+
+            return output;
+        }
+    }
+}
---
+++ ExtensionMethodsMiniApp/ExtensionMethods/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonModel person = new PersonModel();
+
+            person.FirstName = "What is your first name?".RequestString();
+
+            person.LastName = "What is your lastname?".RequestString();
+
+            person.Age = "What is your age?".RequestInt(12, 120);
+
+            person.NumberOfChildren = "How many children do you have?".RequestInt();
+
+
+            Console.WriteLine($"Name: {person.FirstName}, LastName: {person.LastName}, Age: {person.Age}, Children: {person.NumberOfChildren}");
+            Console.ReadLine();
+        }
+    }
+}
