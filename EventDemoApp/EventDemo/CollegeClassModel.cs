5844cbd7a482bd3f60f62e8ab1b88a7453f2e3be
Clau Ziemba <53158588+cziemba1@users.noreply.github.com>
2020-08-16 21:59:32 -0300

    Separation of classes

---
+++ EventDemoApp/EventDemo/CollegeClassModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EventDemo
+{
+    public class CollegeClassModel
+    {
+        public event EventHandler<string> EnrollmentFull;
+        public List<string> enrolledStudents { get; set; } = new List<string>();
+        public List<string> waitingList { get; set; } = new List<string>();
+
+        public string CourseTitle { get; set; }
+        public int MaximunStudents { get; set; }
+
+        public CollegeClassModel(string title, int maximunStudents)
+        {
+            CourseTitle = title;
+            MaximunStudents = maximunStudents;
+        }
+
+        public string SignUpStudents(string studentName)
+        {
+            string output = "";
+
+            if (enrolledStudents.Count < MaximunStudents)
+            {
+                enrolledStudents.Add(studentName);
+                output = $"{studentName} was enrolled in {CourseTitle}.";
+
+                if (enrolledStudents.Count == MaximunStudents)
+                {
+                    EnrollmentFull?.Invoke(this, $"{CourseTitle} enrollment is now full.");
+                }
+            }
+            else
+            {
+                waitingList.Add(studentName);
+                output = $"{studentName} was added to the waiting list in {CourseTitle}.";
+            }
+
+            return output;
+        }
+    }
+}
--- EventDemoApp/EventDemo/Program.cs
+++ EventDemoApp/EventDemo/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -42,7 +41,7 @@ namespace EventDemo
             CollegeClassModel model = (CollegeClassModel) sender;
             Console.WriteLine();
             //Console.WriteLine($"The enrollment is full for {e}");
-            Console.WriteLine($"{model.CourseTitle}: full.");
+            Console.WriteLine($"{model.CourseTitle}: Full.");
             Console.WriteLine();
         }
 
@@ -60,52 +59,5 @@ namespace EventDemo
         //    Console.WriteLine("The enrollment is full for history.");
         //    Console.WriteLine();
         //}
-    }
-
-    public static class ConsoleHelpers
-    {
-        public static void PrintToConsole(this string message)
-        {
-            Console.WriteLine(message);
-        }
-    }
-
-    public class CollegeClassModel
-    {
-        public event EventHandler<string> EnrollmentFull;
-        public List<string> enrolledStudents { get; set; } = new List<string>();
-        public List<string> waitingList { get; set; } = new List<string>();
-
-        public string CourseTitle { get; set; }
-        public int MaximunStudents { get; set; }
-
-        public CollegeClassModel(string title, int maximunStudents)
-        {
-            CourseTitle = title;
-            MaximunStudents = maximunStudents;
-        }
-
-        public string SignUpStudents(string studentName)
-        {
-            string output = "";
-
-            if (enrolledStudents.Count < MaximunStudents)
-            {
-                enrolledStudents.Add(studentName);
-                output = $"{studentName} was enrolled in {CourseTitle}.";
-
-                if (enrolledStudents.Count == MaximunStudents)
-                {
-                    EnrollmentFull?.Invoke(this, $"{CourseTitle} enrollment is now full.");
-                }
-            }
-            else
-            {
-                waitingList.Add(studentName);
-                output = $"{studentName} was added to the waiting list in {CourseTitle}.";
-            }
-
-            return output;
-        }
     }
 }
