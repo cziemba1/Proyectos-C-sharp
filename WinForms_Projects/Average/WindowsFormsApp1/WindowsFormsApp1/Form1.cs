2ac39f3b588f5c5e7079d22908e219b0eaefe02f
Clau Ziemba <53158588+cziemba1@users.noreply.github.com>
2020-09-07 23:03:00 -0300

    Add files via upload

---
+++ WinForms_Projects/Average/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+         int firstNumber;
+         int secondNumber;
+         int thirdNumber;
+         double average;
+        private void BtnAverage_Click(object sender, EventArgs e)
+        {
+            firstNumber = int.Parse(Microsoft.VisualBasic.Interaction.InputBox("Por favor ingrese un numero", "Averages", "", 120,120));
+
+            secondNumber =
+                int.Parse(Microsoft.VisualBasic.Interaction.InputBox("Por favor igrese otro numero", "Averages", "",
+                    120, 120));
+
+            thirdNumber =
+                int.Parse(Microsoft.VisualBasic.Interaction.InputBox("POr favor ingrese otro numero", "Averages", "",
+                    120, 120));
+
+            average = (double)(firstNumber + secondNumber + thirdNumber) / 3;
+
+            MessageBox.Show($"Your average is {average}");
+        }
+    }
+}
